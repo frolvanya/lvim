e8539f5cb057dfbcce933de268cf63cc40e1457b
Ivan Frolov <frolvanya@gmail.com>
2024-05-16 21:29:14 -0400

    chore: disabled backdrop

--- lua/frolik/set.lua
+++ lua/frolik/set.lua
@@ -40,3 +40,5 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+
+lvim.lazy.opts.ui.backdrop = 100

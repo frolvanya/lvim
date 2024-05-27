d296ec7a263907ec441eb92a7e459b20cf56b3a7
Ivan Frolov <frolvanya@gmail.com>
2024-05-27 01:12:59 -0400

    feat: added mappings for typst

---
+++ after/ftplugin/typst.lua
@@ -0,0 +1,6 @@
+lvim.builtin.which_key.mappings["t"] = {
+    name = "Typst",
+    p = { "<cmd>TypstPreview slide<cr>", "TypstPreview" },
+    s = { "<cmd>TypstPreviewStop<cr>", "TypstPreviewStop" },
+    t = { "<cmd>TypstPreviewToggle<cr>", "TypstPreviewToggle" },
+}
